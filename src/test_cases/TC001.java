d9339bd31830fead7487f27507fddd4a56874635
seymaergun <seymaergun89@gmail.com>
2022-12-08 12:05:47 +0100

    updated tc001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -3,5 +3,8 @@ package test_cases;
 public class TC001 {
     public static void main(String[] args) {
         //testing sth
+        //more testing
+        //more and more
+        //kjdfl
     }
 }
